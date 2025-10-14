ad290658b19b5bfef9b24a834c14c3161765186d
Ernesto Gomez <68135929+Nehsto@users.noreply.github.com>
2025-10-13 22:21:49 -0400

    first* commit

---
+++ include/ILI9341.h
@@ -0,0 +1,51 @@
+#ifndef ILI9341_H
+#define ILI9341_H
+
+#include "ILI9341_hw.h"
+
+enum ROTATION{
+    HORIZONTAL  = 0x08, //0x08 0x48 
+    //VERTICAL    = 0x68  //0x68 0x28 NOT DEFINED RIGHT NOW, TODO
+};
+
+//public
+
+void ILI9341_init(const uint8_t rotation);//spi_inst_t *spi_port, uint dc, uint rst, uint miso, uint cs, uint sck, uint mosi);
+
+void ILI9341_set_rotation(const uint8_t rotation);
+
+void ILI9341_draw_rect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t color);
+
+void ILI9341_demo();
+
+//private
+
+void ILI9341_set_command(uint8_t cmd);
+
+void ILI9341_command_param(uint8_t param);
+
+void ILI9341_write_data(const void *data, size_t len); // len - lenght of data in bytes 
+
+void ILI9341_set_out_writing(const uint16_t start_x, const uint16_t end_x, const uint16_t start_y, const uint16_t end_y);
+
+void ILI9341_clear();
+
+void ILI9341_hw_reset();
+
+void ILI9341_sw_reset();
+
+uint16_t RGB_to_16bit(uint8_t r, uint8_t g, uint8_t b);
+
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(ILI9341_CS_PIN, 0); // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(ILI9341_CS_PIN, 1);
+    asm volatile("nop \n nop \n nop");
+}
+
+#endif //ILI9341
---
+++ include/ILI9341_hw.h
@@ -0,0 +1,46 @@
+#ifndef ILI9341_HW_H
+#define ILI9341_HW_H
+
+// --- SPI ---
+#define ILI9341_SPI_PORT    spi0
+#define ILI9341_DC_PIN      21
+#define ILI9341_RST_PIN     20
+#define ILI9341_MISO_PIN    16
+#define ILI9341_CS_PIN      17
+#define ILI9341_SCK_PIN     18
+#define ILI9341_MOSI_PIN    19
+
+#define DATA_BIT            1
+#define COMMAND_BIT         0
+
+#define MHz 1000000UL
+
+#define CMD_SLEEP_MS        5
+#define HW_RESET_SLEEP_MS   10      // Should be 5ms but just to be safe
+#define BOOTUP_SLEEP_MS     150     // Should be 120ms but just to be safe
+
+#define ILI9341_SWRESET     0x01
+#define ILI9341_GAMMASET    0x26
+#define ILI9341_GMCTRP1     0xE0    // Positive Gamma Correction:
+#define ILI9341_GMCTRN1     0xE1    // Negative Gamma Correction:
+#define ILI9341_MADCTL      0x36    // Memory Access Control:
+#define ILI9341_PIXFMT      0x3A    // Pixel Format: 0x00 first, then 0x55 (16 bit) or 0x66 (18 bit)
+#define ILI9341_FRMCTL1     0xB1    // Frame Rate Control: 
+#define ILI9341_SLEEPOUT    0x11
+#define ILI9341_IDLEOFF     0x38
+#define ILI9341_DISPLAYOFF  0x28
+#define ILI9341_DISPLAYON   0x29
+
+#define ILI9341_RGBISC      0xB0    // RGB Interface Signal Control: RCM Bits set to '0b10' to write colors to display
+#define ILI9341_CASET       0x2A    // Column Address Set
+#define ILI9341_PASET       0x2B    // Page Address Set
+#define ILI9341_RAMWR       0x2C    // Memory Write
+
+#define ILI9341_WIDTH       320  
+#define ILI9341_HEIGHT      240 
+
+#include <pico/stdlib.h>
+#include "hardware/spi.h"
+#include "pico/binary_info.h"
+
+#endif
